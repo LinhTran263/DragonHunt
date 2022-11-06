07a1fce2f5dd5ecef60b1eceb61f2fffcab2252b
yahiabeethoven <ybt214@nyu.edu>
2022-11-06 15:34:38 +0400

    setup p5JS style on site

--- public/app.js
+++ public/app.js
@@ -1,5 +1,14 @@
 let socket = io();
 
+function setup() {
+    let canvas = createCanvas(windowWidth, windowHeight);
+    canvas.parent("game_container");
+  }
+  
+  function draw() {
+    background(220);
+  }
+
 socket.on("connect", ()=>{
     console.log("Connection established to server via socket");
 });
