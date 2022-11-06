dfb31870f0ecaf233d4c84a6fe4a04aeff042b9b
LinhTran263 <90758691+LinhTran263@users.noreply.github.com>
2022-11-06 15:48:15 +0400

    moving by keys

--- public/app.js
+++ public/app.js
@@ -1,14 +1,35 @@
 let socket = io();
 
+socket.on("connect", ()=>{
+    console.log("Connection established to server via socket");
+});
+
+
 function setup() {
     let canvas = createCanvas(windowWidth, windowHeight);
     canvas.parent("game_container");
   }
-  
-  function draw() {
-    background(220);
-  }
 
-socket.on("connect", ()=>{
-    console.log("Connection established to server via socket");
-});
+
+let x=200;
+let y=200;
+let d=40;
+  
+function draw() {
+    background(220);
+    if (keyIsPressed){
+    if (keyCode == LEFT_ARROW){
+        x--;
+    }
+    else if (keyCode == RIGHT_ARROW){
+        x++;
+    }
+    else if (keyCode == UP_ARROW){
+        y--;
+    }
+    else if(keyCode == DOWN_ARROW){
+        y++;
+    }
+    }
+    ellipse(x,y,d);
+}
